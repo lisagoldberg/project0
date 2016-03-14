76a865cf184fe7a562a6085b9283255a2d4686f9
Lisa Goldberg <lisa.yogajr@aol.com>
2016-03-13 18:34:20 -0700

    changes as of 6:35pm

--- index.html/script.js
+++ index.html/script.js
@@ -1,53 +1,54 @@
-var cells = ["cell0","cell1","cell2","cell3","cell4","cell5","cell6","cell7","cell8"]
-
-
-
-
 function startGame(){
-  document.turn = "Player X";
+for (var i =1; i <=9, i = i +1){
+  clearBox(i);
+}
 
-  setMessage(document.turn + " gets to start.")
+
+
+  document.turn = "x";
+  if (Math.random()<0.5){
+  document.turn = "o";
+  document.winner = null;
+  setMessage(document.turn + " gets to start.");
 }
 
 function setMessage(msg){
-document.getElementById("message").innerText=msg;)
-
+document.getElementById("message").innerText=msg;
 }
 
 
 function nextMove(square) {
-  if (square.innerText === ""){
-  square.innerText = document.turn;
-  switchTurn ();
-} else {
+  if (document.winner !=== null){
+  setMessage (document.winner + " already won the game.")
+} else if{square.innerText === "")
   setMessage ("That square is already used.");
 }
 
 function switchTurn(){
-if (document.turn ==="x")
-  document.turn === "o"
-else
-  document.turn = "x"
-
-setMessage ("It's " document.turn + " turn.")
-
+if (checkForWinner(document.turn)){
+  setMessage ("Congratulations," + document.turn + "! You win!");
+  document.winner = document.turn;
+}else if(document.turn === "x"){
+  document.turn = "O"
+  setMessage ("It's " + document.turn + "  's turn.")
+}else{
+document.turn = "x";
+setMessage("It's " + document.turn + " 's turn.");
 }
 
-function clearTable(elementId)
-{
-  document.getElementById(elementId).innerHTML = "";
+function checkForWinner(move){
+  var result = false;
+  if (checkRow(1,2,3,move)||
+      checkRow(4,5,6,move)||
+      checkRow(7,8,9,move)||
+      checkRow(1,5,9,move)||
+      checkRow(3,5,7,move)||
+      checkRow(1,4,7,move)||
+      checkRow(2,5,8,move)||
+      checkRow(3,6,9,move)){
+
+    result= true;
 }
-
-
-
-
-
-
-
-
-
-
-
 
 }
 
